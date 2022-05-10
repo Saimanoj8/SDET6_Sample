6fa369bddabcda90ae76f2f4455869751205ce2d
Saimanoj8 <97695405+Saimanoj8@users.noreply.github.com>
2022-05-10 17:19:08 +0530

    Update ReadingDataFromCommandLineTest.java

--- src/test/java/MavenExample/ReadingDataFromCommandLineTest.java
+++ src/test/java/MavenExample/ReadingDataFromCommandLineTest.java
@@ -7,7 +7,7 @@ public class ReadingDataFromCommandLineTest {
 	@Test(groups="smoke")
 	public void readingdatafromcommandlineTest()
 	{
-	
+	        System.out.println("Execute poll SCM in jenkins");
 		String URL = System.getProperty("url");
 		String UN = System.getProperty("username");
 		String PW = System.getProperty("password");
@@ -19,6 +19,7 @@ public class ReadingDataFromCommandLineTest {
 	@Test(groups="regression")
 	public void regressionTest()
 	{
+		  System.out.println("Execute poll SCM in jenkins");
 		System.out.println("it is regression test");
 	}
 }
